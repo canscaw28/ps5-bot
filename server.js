b09f44f021436ffa428ae0080e25680540eaaa32
Craig Weiss <craigweiss28@gmail.com>
2021-07-02 03:57:33 -0700

    dumb cron

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 import cron from 'node-cron';
 import express from 'express';
+import puppeteer from 'puppeteer';
   
 const app = express();
 
-cron.schedule("*/10 * * * * *", () => {
-    console.log("test");
+const targetURL = 'https://www.target.com/p/playstation-5-console/-/A-81114595';
+
+// Hacky Target Alert
+const setupBrowser = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(targetURL);
+
+  // Wait for page content to load
+  await page.waitForTimeout(3000);
+  const html = await page.evaluate(() => document.querySelector('*').outerHTML);
+  console.log(html)
+
+  await browser.close();
+}
+
+cron.schedule('*/30 * * * * *', async () => {
+    console.log('test');
+    await setupBrowser();
 });
   
 app.listen(3000);
